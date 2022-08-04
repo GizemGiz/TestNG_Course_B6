41d8c5e7129f257eaebd247e30bcbad0f9a9b2a3
gizem <gizemmcevirgen@gmail.com>
2022-08-03 20:49:34 -0500

    TestNG - @BeforeMethod and @AfterMethod Annotation

---
+++ src/Day1/_2_TextNG_BeforeAfterMethod.java
@@ -0,0 +1,29 @@
+package Day1;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class _2_TextNG_BeforeAfterMethod {
+
+    @BeforeMethod //wil get executed before each @Test
+    public void beforeMethod(){
+        System.out.println("Before each @Test");
+    }
+
+    @AfterMethod // wil get executed after each @Test
+    public void afterMethod(){
+        System.out.println("After each @Test");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2");
+    }
+}
