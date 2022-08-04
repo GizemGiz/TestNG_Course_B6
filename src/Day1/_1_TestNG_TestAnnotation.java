ce673e143ac91b5aab02583641b70e2364cc01d6
gizem <gizemmcevirgen@gmail.com>
2022-08-03 20:35:23 -0500

    TestNG - @test Annotation

--- src/Day1/_1_TestNG_TestAnnotation.java
+++ src/Day1/_1_TestNG_TestAnnotation.java
@@ -18,8 +18,8 @@ public class _1_TestNG_TestAnnotation {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\gizem\\Selenium Tool\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://techno.study/");
+
 
         String actualURL = driver.getCurrentUrl();
 
@@ -49,6 +49,31 @@ public class _1_TestNG_TestAnnotation {
         Assert.assertEquals(actualURL, expectedURL);
 
         driver.quit();
+
+    }
+
+    @Test
+
+    public void test3(){
+
+
+        String expectedURL = "https://techno.study/sdet";
+
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://techno.study/");
+
+        WebElement aboutUsButton = driver.findElement(By.xpath("//a[text()='About Us']"));
+        aboutUsButton.click();
+
+        String actualURL = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualURL, expectedURL);
+
+        driver.quit();
+
 
     }
 
